dc210b064c057ec1a119d0835c5d04ffc413d82c
small_dk <small_dk@DESKTOP-E5BQ6VB>
2023-03-19 18:13:03 +0800

    加入專案檔案。

---
+++ itsa12/itsa12.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>     
+using namespace std;
+
+int f(int n) {
+    if (n == 0 || n == 1) {
+        return n + 1;
+    }
+    else if (n > 1) {
+        return f(n - 1) + f(n / 2);
+    }
+}
+
+int main() {
+    //ios::sync_with_stdio(0);    
+    //cin.tie(0);    
+    int n;
+    cin >> n;
+    cout << f(n) << "\n";
+
+    return 0;
+}
